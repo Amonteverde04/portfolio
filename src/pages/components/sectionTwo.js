ed3f65e6db601f557ae46d777e80ab55de8a84f3
Alex Monteverde <amonteverde98@gmail.com>
2025-07-04 14:08:18 -0400

    Add link to work history websites. Further fix escaped characters

--- src/pages/components/sectionTwo.js
+++ src/pages/components/sectionTwo.js
@@ -25,14 +25,14 @@ export default function SectionTwo() {
             <GridItem w='100%' h='100%'>
               <Stack>
                 <Heading>What are my current goals?</Heading>
-                <Text fontSize={"1.25rem"}>I love to be creative and watch my ideas come to life. That&quot;s why I am looking to learn as much as I can about AI, while trying to make a positive impact on the world. Even, if it&apos;s a small impact!</Text>
+                <Text fontSize={"1.25rem"}>I love to be creative and watch my ideas come to life. That&#39;s why I am looking to learn as much as I can about AI, while trying to make a positive impact on the world. Even, if it&apos;s a small impact!</Text>
                 <Text fontSize={"1.25rem"}>Lately, I have been diving deep into developing applications powered by LLMs using <span style={{color: "#3571a3"}}><b>Python</b></span>, <span style={{color: "#1C3C3C"}}><b>LangChain</b></span>, and <span style={{color: "#ff6446"}}><b>Chroma</b></span>.</Text>
               </Stack>
             </GridItem>
             <GridItem w='100%' h='100%'>
               <Stack>
                 <Heading>Who am I outside of work?</Heading>
-                <Text fontSize={"1.25rem"}>I am passionate about building software, and learning how to use AI and software to revolutionize the everyday person&quot;s life.</Text>
+                <Text fontSize={"1.25rem"}>I am passionate about building software, and learning how to use AI and software to revolutionize the everyday person&#39;s life.</Text>
                 <Text fontSize={"1.25rem"}>In my free time you can typically find me programming, line dancing, making music, playing video games or spending time with my family and friends.</Text>
               </Stack>
             </GridItem>
